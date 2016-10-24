f88f58ef134c7d0852b5e0b3acf83b5a7b0c1cf3
TaoufikBouabid <bouabid.tawfik@gmail.com>
2016-10-24 23:41:16 +0100

    Initial commit

---
+++ app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ShoppingListApplication.java
@@ -0,0 +1,21 @@
+package com.udacity.firebase.shoppinglistplusplus;
+
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Includes one-time initialization of Firebase related code
+ */
+public class ShoppingListApplication extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+
+
+        super.onCreate();
+
+
+    }
+
+}
